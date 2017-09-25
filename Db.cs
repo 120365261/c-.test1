01bbb10ae5adc1680cb723d7017d380592ccf3a5
fuliyun <120365261@qq.com>
2017-09-25 09:59:54 +0800

    111111

---
+++ Db.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Db : IDisposable
+    {
+        string token = null;
+        public Db()
+        {
+            token = DateTime.Now.Ticks.ToString();
+            Console.WriteLine("created");
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("disposed");
+
+            var logger = NLog.LogManager.GetLogger("file1");
+            logger.Info("disposed");
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("hello:" + token);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,160 @@
-﻿using System;
+﻿using Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ZnlSoft.Utility;
 
-namespace test
+namespace Test
 {
+    public enum EDbs
+    {
+        MiniAccount,
+        zDCData,
+        zErpMini,
+        ZStatisData
+    }
+
+    public class TestService
+    {
+        public string InstanceMethod(string name, int num1)
+        {
+            return name + "---" + num1;
+        }
+
+        private string InstanceMethod()
+        {
+            return "私有实例方法";
+        }
+
+        public static int StaticMethod()
+        {
+            return 1;
+        }
+        private static int StaticMethod(int a = 11111)
+        {
+            return a;
+        }
+    }
+
+    public class IocContainer
+    {
+        static IocContainer()
+        {
+            Program.InitIoc();
+
+        }
+        public static IContainer Container { get; set; }
+        public static T Resolve<T>()
+        {
+            var obj = Container.Resolve<T>();
+            return obj;
+        }
+    }
+
     class Program
     {
+        private static IContainer Container { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");xx
+            //TestInvoke();
+            //NlogTest();
+            InitIoc();
+
+            ResolveTest();
+
+            ResolveTest();
+
+            Task.Run(() =>
+            {
+                Console.WriteLine("------------1");
+                ResolveTest();
+            }).Wait();
+
+            Console.WriteLine("------------2");
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var db = scope.Resolve<Db>();
+                db.SayHello();
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void InitIoc()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Db>().AsSelf().InstancePerLifetimeScope();
+            Container = builder.Build();
+            IocContainer.Container = Container;
+        }
+
+        private static void ResolveTest()
+        {
+            var db = Container.Resolve<Db>();
+            db.SayHello();
+        }
+
+        private static void NlogTest()
+        {
+            var logger = NLog.LogManager.GetLogger("file1");
+            for (int i = 0; i < 10; i++)
+            {
+                logger.Info(i.ToString());
+            }
+            Console.WriteLine("OK");
+        }
+
+        private static void TestInvoke()
+        {
+            string assemblyName = "Test.exe";
+            string className = typeof(TestService).FullName;
+            string staticMethodName = typeof(TestService).GetMethod("StaticMethod").Name;
+            string instanceMethodName = typeof(TestService).GetMethod("InstanceMethod").Name;
+
+            Invoke(assemblyName, className, staticMethodName);
+            Invoke(assemblyName, className, instanceMethodName, DateTime.Now.Millisecond, 520);
+        }
+
+        private static void Invoke(string assemblyName, string classFullName, string methodFullName, params object[] inputParames)
+        {
+            var assembly = Assembly.LoadFrom(assemblyName);
+            var cls = assembly.GetType(classFullName);
+            var method = cls.GetMethod(methodFullName);
+
+            object obj = null;
+            if (!method.IsStatic)
+            {
+                obj = assembly.CreateInstance(classFullName);
+            }
+
+            var methodParams = method.GetParameters();
+            if (methodParams.Length > 0 && inputParames != null)
+            {
+                var loop_i = 0;
+                foreach (var param in methodParams)
+                {
+                    if (inputParames.Length >= loop_i + 1)
+                    {
+                        inputParames[loop_i] = Convert.ChangeType(inputParames[loop_i], param.ParameterType);
+                        loop_i++;
+                    }
+                }
+            }
+
+            var result = method.Invoke(obj, inputParames);
+
+            Console.WriteLine(result);
         }
     }
 }
