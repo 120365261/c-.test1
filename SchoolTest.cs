96294ac688ce8146c3a883e0644f56cc7934a8c7
fuliyun <120365261@qq.com>
2017-09-26 18:06:39 +0800

    324

---
+++ SchoolTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class SchoolTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void DelTest()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void UpdateTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
